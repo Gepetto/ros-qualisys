26f9fc4400f3f00c19363e0597628016757fc2af
Maximilien Naveau <maximilien.naveau@gmail.com>
2022-07-19 00:36:38 +0200

    finish the first draft

--- include/ros-qualisys/qualisys-to-ros.hpp
+++ include/ros-qualisys/qualisys-to-ros.hpp
@@ -5,7 +5,14 @@
 #ifndef ROS_QUALISYS_QUALISYS_TO_ROS_HPP
 #define ROS_QUALISYS_QUALISYS_TO_ROS_HPP
 
+#include <geometry_msgs/TransformStamped.h>
 #include <ros/ros.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include "RTPacket.h"
+#include "RTProtocol.h"
 
 namespace ros_qualisys {
 
@@ -17,10 +24,53 @@ class QualisysToRos {
 
   void setNodeHandle(ros::NodeHandle& nh);
 
-  void initialize();
+  bool initialize();
+
+  void run();
+
+  void terminate();
+
+ private:
+  bool connect();
 
  private:
   ros::NodeHandle* node_handle_;
+
+  // Qualisys objects:
+  CRTProtocol crt_protocol_;
+
+  // params
+  std::string server_address_;
+  unsigned short base_port_;
+  unsigned short udp_port_;
+  int minor_version_;
+  int major_version_;
+  bool big_endian_;
+  std::vector<std::string> model_list_;
+  int frame_rate_;
+  double max_accel_;
+  bool publish_tf_;
+  std::string fixed_frame_id_;
+
+  // sanity check variables
+  bool data_available_ = false;
+
+  // acquiring data
+  CRTPacket::EPacketType packet_type_;
+  CRTPacket* rt_packet_;
+  unsigned int body_count_;
+  const char* body_name_;
+  float px_;
+  float py_;
+  float pz_;
+  std::array<float, 9> rotation_matrix_;
+
+  // ROS interface
+  std::shared_ptr<ros::Rate> rate_;
+  tf2::Matrix3x3 ros_rotation_matrix_;
+  tf2::Quaternion ros_quaternion_;
+  geometry_msgs::TransformStamped ros_transform_;
+  tf2_ros::TransformBroadcaster publisher_;
 };
 
 }  // namespace ros_qualisys
--- src/qualisys-to-ros.cpp
+++ src/qualisys-to-ros.cpp
@@ -4,6 +4,8 @@
 
 #include "ros-qualisys/qualisys-to-ros.hpp"
 
+#include <cmath>
+
 namespace ros_qualisys {
 
 QualisysToRos::QualisysToRos() {}
@@ -14,6 +16,146 @@ void QualisysToRos::setNodeHandle(ros::NodeHandle& nh) {
   node_handle_ = std::addressof(nh);
 }
 
-void QualisysToRos::initialize() {}
+bool QualisysToRos::initialize() {
+  assert(node_handle_ && "Wrong initialization of the node handle.");
+  // The base port (as entered in QTM, TCP/IP port number, in the RT output tab
+  // of the workspace options
+
+  int tmp_base_port(0);
+  int tmp_udp_port(0);
+  node_handle_->param("server_address", server_address_,
+                      std::string("192.168.0.1"));
+  node_handle_->param("server_base_port", tmp_base_port, 22222);
+  node_handle_->param("server_udp_port", tmp_udp_port, 6734);
+  node_handle_->param("major_version", major_version_, 1);
+  node_handle_->param("minor_version", minor_version_, 20);
+  node_handle_->param("model_list", model_list_, std::vector<std::string>(0));
+  node_handle_->param("frame_rate", frame_rate_, 100);
+  node_handle_->param("fixed_frame_id", fixed_frame_id_, std::string("mocap"));
+  base_port_ = static_cast<unsigned short>(tmp_base_port);
+  udp_port_ = static_cast<unsigned short>(tmp_udp_port);
+
+  rate_ = std::make_shared<ros::Rate>(frame_rate_);
+
+  // sanity variables
+  data_available_ = false;
+
+  // Connecting to the server
+  ROS_INFO_STREAM("QualisysToRos::initialize(): Connecting to the Qualisys "
+                  << "Motion Tracking system specified at: " << server_address_
+                  << ":" << base_port_);
+  connect();
+  return true;
+}
+
+bool QualisysToRos::connect() {
+  data_available_ = false;
+
+  // connect properly
+  int failure = 0;
+  while (failure < 20 && ros::ok()) {
+    if (!crt_protocol_.Connected()) {
+      if (!crt_protocol_.Connect(server_address_.c_str(), base_port_,
+                                 &udp_port_, major_version_, minor_version_,
+                                 big_endian_)) {
+        ROS_INFO_STREAM(
+            "QualisysToRos::connect(): Error while connecting: %s\n\n"
+            << crt_protocol_.GetErrorString());
+        ++failure;
+        sleep(1);
+        continue;
+      }
+    }
+
+    if (!data_available_) {
+      if (!crt_protocol_.Read6DOFSettings(data_available_)) {
+        ROS_INFO_STREAM("crt_protocol_.Read6DOFSettings: "
+                        << crt_protocol_.GetErrorString());
+        ++failure;
+        sleep(1);
+        continue;
+      }
+    }
+
+    if (!crt_protocol_.StreamFrames(CRTProtocol::RateAllFrames, 0, 0, NULL,
+                                    CRTProtocol::cComponent6d)) {
+      ROS_INFO_STREAM(
+          "crt_protocol_.StreamFrames: " << crt_protocol_.GetErrorString());
+      ++failure;
+      sleep(1);
+      continue;
+    }
+  }
+  return failure >= 20;
+}
+
+void QualisysToRos::run() {
+  // here again?
+  // connect();
+
+  rt_packet_ = crt_protocol_.GetRTPacket();
+  body_count_ = rt_packet_->Get6DOFBodyCount();
+
+  ROS_INFO_STREAM("Number of bodies found :" << body_count_);
+
+  if (crt_protocol_.Receive(packet_type_, true) ==
+      CNetwork::ResponseType::success) {
+    switch (packet_type_) {
+      case CRTPacket::PacketError:
+        ROS_ERROR_STREAM_THROTTLE(
+            1, "QualisysToRos::run(): Error when streaming frames: "
+                   << crt_protocol_.GetRTPacket()->GetErrorString());
+        break;
+
+      case CRTPacket::PacketNoMoreData:
+        ROS_WARN_STREAM_THROTTLE(1, "No more data");
+        break;
+
+      case CRTPacket::PacketData:
+        if (body_count_ <= 0) {
+          ROS_WARN_THROTTLE(1, "QualisysToRos::run(): No Bodies Found");
+        } else {
+          for (unsigned int i = 0; i < body_count_; i++) {
+            if (rt_packet_->Get6DOFBody(i, px_, py_, pz_,
+                                        rotation_matrix_.data())) {
+              body_name_ = crt_protocol_.Get6DOFBodyName(i);
+              ros_rotation_matrix_.setValue(
+                  rotation_matrix_[0], rotation_matrix_[1], rotation_matrix_[2],
+                  rotation_matrix_[3], rotation_matrix_[4], rotation_matrix_[5],
+                  rotation_matrix_[6], rotation_matrix_[7],
+                  rotation_matrix_[8]);
+              ros_rotation_matrix_.getRotation(ros_quaternion_);
+              ros_quaternion_.normalize();
+              ros_quaternion_[3] = -ros_quaternion_[3];
+              ros_quaternion_.normalize();
+              ros_transform_.transform.translation.x = px_ / 1000.0;
+              ros_transform_.transform.translation.y = py_ / 1000.0;
+              ros_transform_.transform.translation.z = pz_ / 1000.0;
+              ros_transform_.transform.rotation.x = ros_quaternion_.x();
+              ros_transform_.transform.rotation.y = ros_quaternion_.y();
+              ros_transform_.transform.rotation.z = ros_quaternion_.z();
+              ros_transform_.transform.rotation.w = ros_quaternion_.w();
+              ros_transform_.header.frame_id = "Qualisys";
+              ros_transform_.header.stamp = ros::Time::now();
+              ros_transform_.child_frame_id = body_name_;
+              publisher_.sendTransform(ros_transform_);
+            }
+          }
+        }
+
+        break;
+
+      default:
+        ROS_ERROR_THROTTLE(1, "Unknown CRTPacket case");
+    }
+  }
+  rate_->sleep();
+}
+
+void QualisysToRos::terminate() {
+  ROS_INFO("QualisysToRos::disconnect(): Shutting down the communication.");
+  crt_protocol_.StreamFramesStop();
+  crt_protocol_.Disconnect();
+}
 
 }  // namespace ros_qualisys
