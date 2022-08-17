dd9e352885d5b7af0bf904f08d34f97ef4ea0132
Maximilien Naveau <maximilien.naveau@gmail.com>
2022-08-17 14:36:10 +0200

    writting some documentation of the package.

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# ros_qualisys
-ROS wrapper around the https://github.com/qualisys/qualisys_cpp_sdk package
+ros_qualisys
+============
+
+This is ROS wrapper around the https://github.com/qualisys/qualisys_cpp_sdk package.
+
+## Getting started
+
+### Dependencies
+
+All dependencies are listed in the [package.xml](https://github.com/Gepetto/ros-qualisys/blob/main/package.xml) file.
+
+For building the package:
+- git
+- doxygen
+- cmake
+
+For executing the package:
+- catkin + roscpp (if with ROS1)
+- ament_cmake + rclcpp (if with ROS2)
+- Python 2 or 3 (Standard Ubuntu 18.04 or higher)
+- boost (Standard Ubuntu 18.04 or higher)
+- qualisys_cpp_sdk (installable via the robotpkg ppa)
+- tf2 (ROS 1 or 2)
+- tf2_ros (ROS 1 or 2)
+- geometry_msgs (ROS 1 or 2)
+
+### Install from source
+
+You can download this package via github:
+
+    git clone --recursive git@github.com:Gepetto/ros-qualisys.git
+
+And then build the package via CMake and Make:
+
+    source /opt/openrobots/setup.bash
+    source /opt/ros/XXXX/setup.bash
+    cd ros-qualisys
+    mkdir _build
+    cd _build
+    cmake .. -DCMAKE_INSTALL_PREFIX=install
+    make
+    make install
+
+This will install the binaries in `ros-qualisys/install`, one should change the
+install path.
+
+### Install from binaries
+
+One can install this package through the robotpkg ppa:
+
+    sudo apt install robotpkg-ros-qualisys
+
+### Execute the node
+
+If you are in the LAAS-CNRS buiilding in the Gerard bauzil room, one can just
+use the currently available roslaunch (available in `launch/`):
+
+    roslaunch ros-qualisys qualisys_bauzil_bringup.launch
+
+The parameter of the node are loaded from the `config/bauzil-qualisys.yaml` file.
+The yaml is documented so please check the file [directly here](https://github.com/Gepetto/ros-qualisys/blob/main/config/bauzil-qualisys.yaml)
+
+## Online documentation
+
+Online documentation is available at TBD.
--- include/ros-qualisys/qualisys-to-ros.hpp
+++ include/ros-qualisys/qualisys-to-ros.hpp
@@ -1,7 +1,3 @@
-/**
- *
- */
-
 #ifndef ROS_QUALISYS_QUALISYS_TO_ROS_HPP
 #define ROS_QUALISYS_QUALISYS_TO_ROS_HPP
 
@@ -14,47 +10,118 @@
 #include "RTPacket.h"
 #include "RTProtocol.h"
 
+/**
+ * @brief Global namespace of the package.
+ */
 namespace ros_qualisys {
 
+/**
+ * @brief This class is a ROS node allowing us to parametrize the connection
+ * to a Qualisys system and export to /tf and /tf_static the streamed objects.
+ *
+ * We use rosparams in order to parametrize this nodes behavior.
+ * In order to fin out the values of these parameters please refer to your
+ * Qualisys system documentation.
+ *
+ * A quick example of YAML file for the parametrization.
+ *
+ * @code{.unparsed}
+ *
+ * ros_qualisys:
+ *   server_address: "127.0.0.1" # This is the ip where the Qualisys system is
+ * streaming. server_base_port: 22222 # This is the machine port where Qualisys
+ * system is streaming. server_udp_port: 6734 # This is the udp port.
+ *   major_version: 1 # Version of the SDK. (to be checked)
+ *   minor_version: 20 # Version of the SDK. (to be checked)
+ *   frame_rate: 1000 # Maximum rate at which the data are read and broadcasted
+ * to ROS. fixed_frame_id: "mocap" # Name of the frame in which the Qualisys
+ * object are expressed in.
+ *
+ * @endcode
+ *
+ * A practical example is available for a real setup in LAAS-CNRS in
+ * ros-qualisys/config/bauzil-qualisys.yaml file.
+ */
 class QualisysToRos {
  public:
+  /**
+   * @brief Construct a new Qualisys To Ros object, this a simple default
+   * constructor.
+   */
   QualisysToRos();
 
+  /**
+   * @brief Destroy the Qualisys To Ros object.
+   */
   ~QualisysToRos();
 
+  /**
+   * @brief Set the Node Handle object.
+   *
+   * @param nh pointer to a node handle.
+   */
   void setNodeHandle(ros::NodeHandle& nh);
 
+  /**
+   * @brief Initialize the object by:
+   *  - getting the ROS params.
+   *  - starting the connection to the Qualisys system using the above params.
+   *
+   * @return true in case of success,
+   * @return false otherwise
+   */
   bool initialize();
 
+  /**
+   * @brief Acquire the data from the Qualisys stream and send it back to the
+   * /tf or /tf_static topic.
+   */
   void run();
 
+  /**
+   * @brief Properly shutdown the connection with the Qualisys system.
+   */
   void terminate();
 
  private:
+  /**
+   * @brief Connect to the Qualisys system and check if the connection went
+   * well. This method is used in the initialization.
+   *
+   * @return true in case of success.
+   * @return false otherwise.
+   */
   bool connect();
 
  private:
+  /// @brief ROS node handle to fetch the ROS params.
   ros::NodeHandle* node_handle_;
 
-  // Qualisys objects:
+  /// @brief Qualisys communication object.
   CRTProtocol crt_protocol_;
 
-  // params
+  // Server parameters.
   std::string server_address_;
   unsigned short base_port_;
   unsigned short udp_port_;
   int minor_version_;
   int major_version_;
   bool big_endian_;
+
+  // Maximum rate at which application is running, can be less.
   int frame_rate_;
-  double max_accel_;
+
+  // Publisher to the /tf ROS topic.
   bool publish_tf_;
+
+  // Name of the fix frame in which the object are expressed in. This is the
+  // mocap frame.
   std::string fixed_frame_id_;
 
-  // sanity check variables
+  // Sanity check variables
   bool data_available_ = false;
 
-  // acquiring data
+  // Qualisys SDK acquiring data objects.
   CRTPacket::EPacketType packet_type_;
   CRTPacket* rt_packet_;
   unsigned int body_count_;
@@ -64,7 +131,7 @@ class QualisysToRos {
   float pz_;
   std::array<float, 9> rotation_matrix_;
 
-  // ROS interface
+  // ROS interface.
   std::shared_ptr<ros::Rate> rate_;
   tf2::Matrix3x3 ros_rotation_matrix_;
   tf2::Quaternion ros_quaternion_;
@@ -75,3 +142,14 @@ class QualisysToRos {
 }  // namespace ros_qualisys
 
 #endif  // ROS_QUALISYS_QUALISYS_TO_ROS_HPP
+
+/** @example ros-qualisys.cpp
+ * This is the actual usage of the QualisysToRos class in a ROS node.
+ * The Node is defining the namespace so that if the node is launched
+ * in a roslaunch file then the namespace will be the "name" of the node.
+ * This way, in order to define the parameters in a yaml file, one need to
+ * gives the same namespace as the node name.
+ *
+ * @see ros-qualisys/config/bauzil-qualisys.yaml
+ * @see ros-qualisys/launch/qualisys_bauzil_bringup.launch
+ */
