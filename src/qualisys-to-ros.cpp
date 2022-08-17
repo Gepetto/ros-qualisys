a68c8f895c2fb6f43c004910015671deee1c86d6
Maximilien Naveau <maximilien.naveau@gmail.com>
2022-08-17 14:36:10 +0200

    writting some documentation of the package.

--- src/qualisys-to-ros.cpp
+++ src/qualisys-to-ros.cpp
@@ -1,7 +1,3 @@
-/**
- *
- */
-
 #include "ros-qualisys/qualisys-to-ros.hpp"
 
 #include <cmath>
--- src/ros-qualisys.cpp
+++ src/ros-qualisys.cpp
@@ -1,32 +1,41 @@
-/**
- *
- */
+/// @example
 
 #include <ros/ros.h>
 
 #include "ros-qualisys/qualisys-to-ros.hpp"
 
 int main(int argc, char* argv[]) {
+  // Classic definition of the node.
   ros::init(argc, argv, "ros-qualisys", ros::init_options::NoSigintHandler);
+
+  // Here it says that the namespace of the Node can be overwritten by the
+  // launcher
   ros::NodeHandle nh("~");
 
+  // The node class.
   ros_qualisys::QualisysToRos q2r;
+  // get the ROS node handle.
   q2r.setNodeHandle(nh);
 
+  // Initialize the communication with the Qualisys system and get the ROS
+  // params.
   if (!q2r.initialize()) {
     ROS_INFO("Initialization of the Qualisys driver failed!");
     return -1;
   }
   ROS_INFO("Successfully initialize Qualisys connection!");
 
+  // Run the communication loop.
   while (ros::ok()) {
     q2r.run();
     ros::spinOnce();
   }
 
+  // Properly shutdown the connexion to Qualisys
   ROS_INFO("Shutting down");
   q2r.terminate();
 
+  // Properly shutting down the ROS node.
   ros::shutdown();
   return 0;
 }
